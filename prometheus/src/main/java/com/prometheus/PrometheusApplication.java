b12818e01722f3da646e1f0e800fb342247750c8
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2024-12-01 03:00:23 +0900

    feat: HTTP 요청과 응답 과거 기록을 확인

--- prometheus/src/main/java/com/prometheus/PrometheusApplication.java
+++ prometheus/src/main/java/com/prometheus/PrometheusApplication.java
@@ -1,7 +1,9 @@
 package com.prometheus;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrometheusApplication {
@@ -9,4 +11,11 @@ public class PrometheusApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrometheusApplication.class, args);
 	}
+
+	// HTTP 요청과 응답 과거 기록을 확인
+	@Bean
+	public InMemoryHttpExchangeRepository httpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository();
+	}
+
 }
