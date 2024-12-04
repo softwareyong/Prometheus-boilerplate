887e91599d010493c58dbb77b6b80c406d14f29b
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2024-12-04 20:57:34 +0900

    chore: import 수정

--- prometheus/src/main/java/com/prometheus/PrometheusApplication.java
+++ prometheus/src/main/java/com/prometheus/PrometheusApplication.java
@@ -1,11 +1,24 @@
 package com.prometheus;
 
+import com.prometheus.order.gauge.StockConfigV1;
+import com.prometheus.order.gauge.StockConfigV2;
+import com.prometheus.order.v0.OrderConfigV0;
+import com.prometheus.order.v1.OrderConfigV1;
+import com.prometheus.order.v2.OrderConfigV2;
+import com.prometheus.order.v3.OrderConfigV3;
+import com.prometheus.order.v4.OrderConfigV4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+//@Import(OrderConfigV0.class)
+//@Import(OrderConfigV1.class)
+//@Import(OrderConfigV2.class)
+//@Import(OrderConfigV3.class)
+@Import({OrderConfigV4.class, StockConfigV2.class})
+@SpringBootApplication(scanBasePackages = "com.prometheus.controller")
 public class PrometheusApplication {
 
 	public static void main(String[] args) {
