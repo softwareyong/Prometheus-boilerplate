3334251696ae6c37b1cfc1d5fe9473ef38e693c5
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2024-12-04 15:47:20 +0900

    feat: 주문 예제

---
+++ prometheus/src/main/java/com/prometheus/order/v0/OrderConfigV0.java
@@ -0,0 +1,15 @@
+package com.prometheus.order.v0;
+
+import com.prometheus.order.OrderService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderConfigV0 {
+
+    @Bean
+    OrderService orderService() {
+        return new OrderServiceV0();
+    }
+
+}
